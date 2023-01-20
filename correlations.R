07abc8763785cdbbbacfa7c10126dd267725d1a3
lfdelgadom <lfdelgadom@unal.edu.co>
2023-01-20 07:44:38 -0500

    Data BCGN1 analysis done, the code is ready

--- correlations.R
+++ correlations.R
@@ -3,6 +3,10 @@
 # -------------------------------------------------------------------------
 
 
+# Libraries ---------------------------------------------------------------
+
+library(corrplot)
+
 # Load the file -----------------------------------------------------------
 
 
@@ -10,13 +14,13 @@
 list_file <- list.files(folder)
 
 sel_file <- list_file[str_detect(list_file, "_master_results_") &
-                        str_detect(list_file, trial_interest)]
+  str_detect(list_file, trial_interest)]
 
 sel_file[1]
 blupDF_kp <- read_excel(
   paste(folder,
-        sel_file[1],
-        sep = ""
+    sel_file[1],
+    sep = ""
   ),
   sheet = paste0("BLUPs_", trial_interest)
 )
@@ -30,42 +34,42 @@ blupDF_value <- blupDF_kp %>%
 
 # Change colnames ---------------------------------------------------------
 
-colnames(blupDF_value) = c("Cooking time (min)", "Dry matter by oven (%)",
-                           "Cyanide content (ppm)", "Plant height (cm)",
-                           "PPD", "Yield (t/ha)")
+colnames(blupDF_value) <- c(
+  "B-carotene by NIR (ug/g)", "Total carotenoids by NIR (ug/g)",
+  "Cooking time (min)", "Dry matter by oven (%)",
+  "Cyanide content (ppm)", "Plant height (cm)", "1st branch height (cm)",
+  "PPD", "Yield (t/ha)"
+)
+
 
 
 # Compute the correlation -------------------------------------------------
 
-M = cor(as.matrix(blupDF_value), use = "complete.obs")
-testRes = cor.mtest(as.matrix(blupDF_value), conf.level = 0.95)
+M <- cor(as.matrix(blupDF_value), use = "complete.obs")
+testRes <- cor.mtest(as.matrix(blupDF_value), conf.level = 0.95)
 
 
 # Save the file -----------------------------------------------------------
 
 pdf(paste("cor_traits_", trial_interest, "_",
-          Sys.Date(),".pdf", sep=""), width = 10, height = 10)
-corrplot(M, p.mat=testRes$p, 
-         method="color", 
-         sig.level = c(0.001, 0.01, 0.05), 
-         insig = 'label_sig', 
-         pch.cex = 0.9, 
-         #addCoef.col = 'black',
-         type = 'upper', 
-         diag = FALSE, 
-         col = colorRampPalette(c("red", "white", "blue"))(40),
-         tl.cex = 1.5, 
-         tl.col = "black",
-         addgrid.col = 'black'
-)$corrPos -> p1; text(p1$x, p1$y, pos =3, round(p1$corr, 3), col = "black", cex=1)
+  Sys.Date(), ".pdf",
+  sep = ""
+), width = 10, height = 10)
+
+corrplot(M,
+  p.mat = testRes$p,
+  method = "color",
+  sig.level = c(0.001, 0.01, 0.05),
+  insig = "label_sig",
+  pch.cex = 0.9,
+  # addCoef.col = 'black',
+  type = "upper",
+  diag = FALSE,
+  col = colorRampPalette(c("red", "white", "blue"))(40),
+  tl.cex = 1.5,
+  tl.col = "black",
+  addgrid.col = "black"
+)$corrPos -> p1
+text(p1$x, p1$y, pos = 3, round(p1$corr, 3), col = "black", cex = 1)
 
 dev.off()
-
-
-
-# correlation among dry matters ------------------------------------------
-
-traits <- readRDS(file = here::here("data_v2", "tidy_PDGN12023-01-17.rds"))
-
-
-cor(traits$DM_gravity, traits$DM_raw, use = "complete.obs")
