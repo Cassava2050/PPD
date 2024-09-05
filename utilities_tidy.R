404b085833bd8147c46d7a8fcfd0ee22a3923f5e
Luis Fernando <lfdelgadom@unal.edu.co>
2024-09-05 17:15:37 -0500

    Update utilities_tidy.R

--- utilities_tidy.R
+++ utilities_tidy.R
@@ -273,7 +273,10 @@ check_clone_name = function(clone_list,
   "BK", "Rayong11", "IBA972205", "IBA920057", "IBA980505", "N30", "MBUNDUMALI",
   "SAUTI", "UNKNOWN1", "UNKOWN2", "Rayong72", "LocalC1", "LocalC2", "KM325", "R5",
   "Hanoi1", "Hanoi2", "Hanoi3", "Hanoi4", "Hanoi5", "AZULITA", "COL9128", "SUGARY", 
-  "Centinela", "C38", "TMS30555", "TMS60444", "PSEXXX1")
+  "Centinela", "C38", "TMS30555", "TMS60444", "PSEXXX1", "89", "HB100", "HB60", "HB90",
+  "IITA920057", "IITA972205", "IITA980505", "IITA980581", "KU50", "TMEB419", "TME3",
+  "C33", "Pirun2", "Pirun4", "IITA0057", "IITA0505", "IITA0581", "IITA2205",
+  "Pirun1", "Pirun3", "R1")
   
   cat("The other known clones:")
   print(sort(known_clone))
